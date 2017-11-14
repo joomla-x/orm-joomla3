d64d5d5ef12785c7a30bdfd741e9237f0fd67746
Niels Braczek <nbraczek@bsds.de>
2017-11-14 17:46:27 +0100

    Added bootstrapping

---
+++ src/ORM.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Joomla3\Glue;
+
+use JConfig;
+use JFactory;
+use Joomla\DI\Container;
+use Joomla\ORM\Service\RepositoryFactory;
+use Joomla\ORM\Service\StorageServiceProvider;
+use Joomla\ORM\Storage\Doctrine\PrefixedDoctrineDataMapper;
+use Psr\Container\ContainerInterface;
+
+defined('_JEXEC') or die;
+
+class ORM
+{
+    public static function bootstrap()
+    {
+        self::createDatabaseConfiguration();
+        $container         = self::createContainer();
+        $repositoryFactory = self::createRepositoryFactory($container);
+        $container->set('repository', $repositoryFactory);
+
+        return $container;
+    }
+
+    /**
+     * Create RepositoryFactory
+     *
+     * @param ContainerInterface $container
+     *
+     * @return RepositoryFactory
+     */
+    protected static function createRepositoryFactory($container)
+    {
+        return (new StorageServiceProvider(JPATH_ROOT . '/config/database.ini'))->createRepositoryFactory($container);
+    }
+
+    /**
+     * Ensure container exists
+     *
+     * @return Container
+     */
+    protected static function createContainer()
+    {
+        $app = JFactory::getApplication();
+
+        if (is_null($app->get('container', null))) {
+            $app->set('container', new Container());
+        }
+
+        $container = $app->get('container');
+
+        if (!method_exists($container, 'set')) {
+            $container = new Container($container);
+        }
+
+        return $container;
+    }
+
+    /**
+     * Create DB configuration
+     */
+    protected static function createDatabaseConfiguration()
+    {
+        $config          = new JConfig();
+        $dsn             = $config->dbtype . '://' . $config->user . ':' . $config->password . '@' . $config->host . '/' . $config->db;
+        $dataMapperClass = PrefixedDoctrineDataMapper::class;
+
+        $ini = <<<INI
+databaseUrl="$dsn"
+definitionPath="entities"
+dataMapper="$dataMapperClass"
+INI;
+        file_put_contents(JPATH_ROOT . '/config/database.ini', $ini);
+    }
+}
